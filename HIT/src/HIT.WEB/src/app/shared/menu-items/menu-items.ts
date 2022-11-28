4111a146a436f947f81368fe73b3a575a6f1c302
Idjaz Hossanee <ihossanee@objectivity.co.uk>
2022-11-28 17:12:39 +0400

    in pr

--- HIT/src/HIT.WEB/src/app/shared/menu-items/menu-items.ts
+++ HIT/src/HIT.WEB/src/app/shared/menu-items/menu-items.ts
@@ -9,10 +9,10 @@ export interface Menu {
 const MENUITEMS = [
   // { name: 'Dashboard', type: 'link', icon: 'av_timer' },
  
-  { name: 'Candidates', type: 'link', icon: 'groups' },
-  { name: 'skill', type: 'link', icon: 'stars' },
-  { name: 'job-adverts', type: 'link', icon: 'work' },
-  { name: 'Administration', type: 'link', icon: 'admin_panel_settings' },
+  { label: 'Candidates', name: 'Candidates', type: 'link', icon: 'groups' },
+  { label: 'Skills', name: 'skill', type: 'link', icon: 'stars' },
+  { label: 'Job Adverts', name: 'job-adverts', type: 'link', icon: 'work' },
+  { label: 'Administration', name: 'Administration', type: 'link', icon: 'admin_panel_settings' },
 ];
 
 @Injectable()
