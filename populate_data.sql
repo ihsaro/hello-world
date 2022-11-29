159813617ee2714a7615179d4d7df5d87965de04
Idjaz Hossanee <ihossanee@objectivity.co.uk>
2022-11-29 14:05:48 +0400

    data

--- populate_data.sql
+++ populate_data.sql
@@ -1,5 +1,3 @@
--- SQLite
-/*
 INSERT INTO Candidates (FirstName, LastName, EmailAddress, LinkedInURL, CandidateType, CandidateLocation, YearsOfExperience, CreatedTimestamp, LastUpdatedTimestamp)
 VALUES
 ('John', 'Stones', 'idjazhossanee@gmail.com', '', 0, 'Mauritius', 2, DATE('now'), DATE('now')),
@@ -12,7 +10,6 @@ VALUES
 ('James', 'Middleschool', 'idjazhossanee@gmail.com', '', 1, 'Germany', 6, DATE('now'), DATE('now')),
 ('Richard', 'Milestone', 'idjazhossanee@gmail.com', '', 2, 'Poland', 5, DATE('now'), DATE('now')),
 ('Gabriel', 'Rock', 'idjazhossanee@gmail.com', '', 0, 'Poland', 3, DATE('now'), DATE('now'));
-*/
 
 INSERT INTO CandidateSkills (CandidateId, Skill, CreatedTimestamp, LastUpdatedTimestamp)
 VALUES
@@ -21,3 +18,42 @@ VALUES
 (2, 'Java', DATE('now'), DATE('now')),
 (2, 'C#', DATE('now'), DATE('now')),
 (2, 'Python', DATE('now'), DATE('now'));
+
+INSERT INTO JobPostings (Title, Description, YearsOfExperience, JobLocation, JobType, JobStatus, CreatedTimestamp, LastUpdatedTimestamp)
+VALUES
+('Associate Software Engineer', '', 2, 0, 0, 0, DATE('now'), DATE('now')),
+('Software Engineer', '', 2, 0, 0, 0, DATE('now'), DATE('now')),
+('Senior Software Engineer', '', 2, 0, 0, 0, DATE('now'), DATE('now')),
+('Technical Architect', '', 2, 0, 0, 0, DATE('now'), DATE('now')),
+('Test Automation Engineer', '', 2, 0, 0, 0, DATE('now'), DATE('now')),
+('QA Engineer', '', 2, 0, 0, 0, DATE('now'), DATE('now')),
+('QA Senior Engineer', '', 2, 0, 0, 0, DATE('now'), DATE('now')),
+('QA Lead Engineer', '', 2, 0, 0, 0, DATE('now'), DATE('now')),
+('Director in Technology', '', 2, 0, 0, 0, DATE('now'), DATE('now')),
+('HR Manager', '', 2, 0, 0, 0, DATE('now'), DATE('now'));
+
+INSERT INTO JobSkills (Name, Description, SkillCategory, CreatedTimestamp, LastUpdatedTimestamp)
+VALUES
+('C#', '', 1, DATE('now'), DATE('now')),
+('Java', '', 1, DATE('now'), DATE('now')),
+('Python', '', 1, DATE('now'), DATE('now')),
+('Angular', '', 1, DATE('now'), DATE('now')),
+('React', '', 1, DATE('now'), DATE('now')),
+('Vue', '', 1, DATE('now'), DATE('now')),
+('Svelte', '', 1, DATE('now'), DATE('now')),
+('Effective Communication', '', 0, DATE('now'), DATE('now')),
+('Effective Mentoring', '', 3, DATE('now'), DATE('now')),
+('Agile Planning', '', 2, DATE('now'), DATE('now'));
+
+INSERT INTO JobPostingSkills(JobPostingId, JobSkillId, CreatedTimestamp, LastUpdatedTimestamp)
+VALUES
+(1, 1, DATE('now'), DATE('now')),
+(1, 3, DATE('now'), DATE('now')),
+(1, 4, DATE('now'), DATE('now')),
+(2, 2, DATE('now'), DATE('now')),
+(2, 5, DATE('now'), DATE('now')),
+(10, 8, DATE('now'), DATE('now')),
+(10, 9, DATE('now'), DATE('now')),
+(6, 1, DATE('now'), DATE('now')),
+(6, 2, DATE('now'), DATE('now')),
+(6, 8, DATE('now'), DATE('now'));
